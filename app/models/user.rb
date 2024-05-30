34902009e7fc2283392e40235667d754bb97a88d
Grégory Huet <gregory@ixmedia.com>
2024-05-30 19:36:16 -0400

    Add user fields validation on update

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  GENDER = [ 'male', 'female', 'non_binary', 'other' ]
+  enum gender: Hash[GENDER.map { |c| [c, c.to_s] }]
+
+  validates :age, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, on: :update
+  validates :gender, presence: true, on: :update
 end
