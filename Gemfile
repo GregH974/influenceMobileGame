ed22dfd250980f5fa9cddbec347accd70d53bd3f
Grégory Huet <gregory@ixmedia.com>
2024-05-30 16:29:50 -0400

    Install and set letter opener web in development environnement

--- Gemfile
+++ Gemfile
@@ -65,6 +65,9 @@ group :development do
 
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
+
+  # Gives letter_opener an interface for browsing sent emails.
+  gem 'letter_opener_web', '~> 3.0'
 end
 
 group :test do
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :letter_opener_web
+
   # Config use by Device gem
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
