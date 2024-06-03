cb131c2415de92f83e16d502bbf80ac0502f9080
Grégory Huet <gregory@ixmedia.com>
2024-06-03 16:03:02 -0400

    Move main nav in a shared template

--- app/controllers/players/base_controller.rb
+++ app/controllers/players/base_controller.rb
@@ -1,5 +1,6 @@
 module Players
   class BaseController < ApplicationController
+    # Use prepend to prevent conflict with before_action in controller in the future
     prepend_before_action :authenticate_user!
   end
 end
