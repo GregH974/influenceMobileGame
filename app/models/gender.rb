0a0585d13262178f5368e1f747358b6e4895ebff
Grégory Huet <gregory@ixmedia.com>
2024-06-03 00:32:14 -0400

    Update models

--- app/models/gender.rb
+++ app/models/gender.rb
@@ -11,18 +11,14 @@ class Gender < ApplicationRecord
   # == Relationships ========================================================
   has_many :offer_genders
   has_many :offers, through: :offer_genders
+  has_many :players
 
   # == Validations ==========================================================
   validates :name, presence: true
-  validates :slug, presence: true
 
   # == Scopes ===============================================================
 
   # == Instance Methods =====================================================
 
   # == Class Methods ========================================================
-  private
-    def generate_slug
-      self.slug = name
-    end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -9,9 +9,11 @@ class Player < ApplicationRecord
 
   # == Relationships ========================================================
   belongs_to :user
-
+  belongs_to :gender
 
   # == Validations ==========================================================
+  validates :age, presence: true, numericality: { only_integer: true }, comparison: { greater_than: 18 }
+  validates :gender_id, presence: true
 
   # == Scopes ===============================================================
 
