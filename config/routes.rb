0f14964cdf135f361ce2263295798406ee8e05c8
Grégory Huet <gregory@ixmedia.com>
2024-06-12 14:45:27 -0400

    Clean route without todo.

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,4 @@ Rails.application.routes.draw do
       end
     end
   end
-
-  # TODO : to complete template
-  get   '404',                              to: 'errors#not_found',               as: 'not_found'
-  get   '503',                              to: 'errors#forbidden',               as: 'forbidden'
-  get   '500',                              to: 'errors#internal_server_error',   as: 'internal_server_error'
 end
